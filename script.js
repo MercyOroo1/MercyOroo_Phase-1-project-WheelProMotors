a3c94ace94cf10ca02aa844ef4266c05f0584af2
Mercy Oroo <mercy.oroo@student.moringaschool.com>
2024-04-12 01:46:39 +0300

    clearing code commit

--- script.js
+++ script.js
@@ -1,58 +1,32 @@
-document.addEventListener("DOMContentLoaded",()=> {
-    let img = document.getElementById("imgcontainer")
-    let slides = ["urus.jpg","lambo.jpeg","slide.jpeg","slidecar.jpeg","tesla.jpeg","exp.jpg"]
-    let start = 0
+document.addEventListener("DOMContentLoaded", () => {
+  let img = document.getElementById("imgcontainer")
+  let slides = ["urus.jpg", "lambo.jpeg", "slide.jpeg", "slidecar.jpeg", "tesla.jpeg", "exp.jpg", "-jpg.webp"]
+  let start = 0
 
-    function slider () {
-        if (start < slides.length) {
-            start += 1
-        } else {
-            start = 1
-        }
-        console.log(img)
-        img.innerHTML = "<img src="+slides[start-1]+">"
-        
+  function slider() {
+    if (start < slides.length) {
+      start += 1
+    } else {
+      start = 1
     }
-    setInterval(slider,5000)
+    console.log(img)
+    img.innerHTML = "<img src=" + slides[start - 1] + ">"
 
+  }
+  setInterval(slider, 5000)
 
-  //  let popButton = document.getElementById("test")
-  // let popWindow = document.querySelector("#payment-form")
-  // let closeButton = document.getElementById("submit")
-  // let submitButton = document.getElementById("submit-form")
-  // console.log(submitButton)
-  // // console.log (popButton)
-  // // console.log (popWindow)
-  // // console.log (closeButton)
-  // popButton.addEventListener ("click", (event)=> { 
-  //   event.preventDefault()
-  // popWindow.style.display = "block"
-
-  // })
-  // closeButton.addEventListener ("click",(e)=> {
-  // e.preventDefault()
-  //   popWindow.style.display = "none"
-  // })
-//  popWindow.addEventListener ("submit", (e) => {
-//   e.preventDefault() 
-//   console.log("Hi")
-//   console.log (e.target.name.value)
-//form updates payment details in server
-//  })
 
   let wishListButton = document.getElementById("wishlist")
   let wishListWindow = document.querySelector("#div-wishlist")
-  let closeWishlist = document.getElementById ("closeWishlist")
-  // console.log(wishListButton)
-  // console.log(wishListWindow)
-  // console.log(closeWishlist)
+  let closeWishlist = document.getElementById("closeWishlist")
 
-  wishListButton.addEventListener("click", (e)=> {
+
+  wishListButton.addEventListener("click", (e) => {
     e.preventDefault()
     wishListWindow.style.display = "block"
   })
-  closeWishlist.addEventListener ("click", () => {
+  closeWishlist.addEventListener("click", () => {
     wishListWindow.style.display = "none"
-  }) 
+  })
 
 })
--- test.js
+++ test.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let listProducts = []
     let wishlistSUV = []
     let wishlistHTML = document.querySelector("#div-wishlist")
-    console.log (wishlistHTML)
+    console.log(wishlistHTML)
 
 
     const addDataToHtml = () => {
@@ -21,10 +21,10 @@ document.addEventListener("DOMContentLoaded", () => {
             <div>  Availabilty : In stock </div>
             <button class= "wishlist"> Add to Wish list </button>
             <button class= "payment"> Get Payment Details </button>`
-           
-            // newProduct.className = "suv"
-            listProduct.appendChild(newProduct)
-            
+
+
+                listProduct.appendChild(newProduct)
+
             })
         }
     }
@@ -34,82 +34,54 @@ document.addEventListener("DOMContentLoaded", () => {
             .then(res => res.json())
             .then(data => {
                 listProducts = data
-                // console.log (listProducts)
+
                 addDataToHtml()
             })
 
     }
     initSuv()
 
-    // let listSport = document.querySelector(".card2")
-    // let listSports = []
 
-    // const addSportsToHtml = () => {
-    //     listSport.innerHTML = ""
-    //     if (listSports.length > 0) {
-    //         listSports.forEach(car => {
-    //             let newSports = document.createElement("div")
-    //             newSports.innerHTML = 
-    //             `<h1> CAR ID: ${car.id} </h1>
-    //             <img src = ${car.image}>
-    //     <h5> ${car.name} </h5>
-    //     <div class = "price"> ${car.price} </div>
-    //     <button class= "wishlist"> Add to Wish list </button>
-    //     <button class= "payment"> Buy car </button>`
-    //     listSport.appendChild(newSports)
+    let wishlistForm = document.querySelector(".wishlist-form")
 
-    //         })
-    //     }
-    // }
-
-    // const initSports = () => {
-    //     fetch("http://localhost:3000/Sports")
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             listSports = data
-    //             addSportsToHtml()
-    //         })
-    // }
-    // initSports()
-let wishlistForm = document.querySelector(".wishlist-form")
-// console.log(wishlistForm)
-let closeWishlistForm = document.querySelector("#close-wishlist-form")
-     listProduct.addEventListener("click", (event)=> {
+    let closeWishlistForm = document.querySelector("#close-wishlist-form")
+    listProduct.addEventListener("click", (event) => {
         event.preventDefault()
         let positionClick = event.target
-         //console.log(positionClick)
-       if (positionClick.classList.contains ("wishlist")) {
-         wishlistForm.style.display = 'block'}
 
-          closeWishlistForm.addEventListener ("click", (e)=> {
-           e.preventDefault()
+        if (positionClick.classList.contains("wishlist")) {
+            wishlistForm.style.display = 'block'
+        }
+
+        closeWishlistForm.addEventListener("click", (e) => {
+            e.preventDefault()
             wishlistForm.style.display = "none"
-         })
+        })
 
-         let form = document.querySelector ("#wishlist-form")
-         form.addEventListener("submit", (e)=> {
+        let form = document.querySelector("#wishlist-form")
+        form.addEventListener("submit", (e) => {
             e.preventDefault()
             const input = document.querySelector("input#wishlisttitle")
             let id = input.value
             console.log(id)
             if (id) {
 
-            fetch (`http://localhost:3000/SUV/${id}`)
-            .then (res => res.json())
-            .then  (data => {
-                console.log(data)
-            addWishlistItem (data,id)
-            renderWishlistItem (data)
-        }
-        )
-            form.reset ()
+                fetch(`http://localhost:3000/SUV/${id}`)
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data)
+                        addWishlistItem(data, id)
+                        renderWishlistItem(data)
+                    }
+                    )
+                form.reset()
 
-     }
+            }
+        })
     })
-         })
-      function addWishlistItem (wishlistObj,id) {
+    function addWishlistItem(wishlistObj, id) {
         console.log(id)
-        let objname = document.createElement ("div")
+        let objname = document.createElement("div")
         objname.innerHTML = `
       <img src = "${wishlistObj.image}">
         <div class = "carName"> ${wishlistObj.name}  </div>
@@ -122,175 +94,106 @@ let closeWishlistForm = document.querySelector("#close-wishlist-form")
         delbtn.style.color = "white"
         objname.className = "div-item"
 
-        delbtn.addEventListener("click", (e)=> {
+        delbtn.addEventListener("click", (e) => {
             e.target.parentNode.remove()
-            fetch (`http://localhost:3000/wishlist/${id}`,{
-                method : "DELETE",
-                headers : {
-                    'Content-Type' : "application.json"
+            fetch(`http://localhost:3000/wishlist/${id}`, {
+                method: "DELETE",
+                headers: {
+                    'Content-Type': "application.json"
                 }
-             })
-             .then (res => res.json())
-             .then (data => console.log(data))
-                
-                
-            
-            
-            
-            
+            })
+                .then(res => res.json())
+                .then(data => console.log(data))
+
+
+
+
+
+
         })
         objname.appendChild(delbtn)
 
         let mainWishlist = document.querySelector("#wishlist-container")
         mainWishlist.appendChild(objname)
-      }
-         function renderWishlistItem (wishlistObj) {
-            fetch ("http://localhost:3000/wishlist", {
-                method : "POST",
-                headers : {
-                    "Content-Type": "application/json",
-                    "Accept" : "application/json"
-                },
-                body: JSON.stringify (wishlistObj)
-            })
+    }
+    function renderWishlistItem(wishlistObj) {
+        fetch("http://localhost:3000/wishlist", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(wishlistObj)
+        })
             .then(res => res.json())
-            .then (data => { console.log(data)})
-         }
- function handledelete (id) {
-    fetch (`http://localhost:3000/wishlist/${id}`,{
-    method : "DELETE",
-    headers : {
-        'Content-Type' : "application.json"
+            .then(data => { console.log(data) })
     }
- })
- .then (res => res.json())
- .then (data => console.log(data))
-    
-    
+    function handledelete(id) {
+        fetch(`http://localhost:3000/wishlist/${id}`, {
+            method: "DELETE",
+            headers: {
+                'Content-Type': "application.json"
+            }
+        })
+            .then(res => res.json())
+            .then(data => console.log(data))
 
 
-}
 
-let paymentButton = document.querySelector(".test")
-console.log(paymentButton)
-let popForm = document.querySelector("#payment-form")
-console.log(popForm)
-let closeForm = document.querySelector("#submit")
-console.log (closeForm)
 
-listProduct.addEventListener("click", (event)=> {
-    event.preventDefault()
-    let positionClick2 = event.target
-     console.log(positionClick2)
-   if (positionClick2.classList.contains ("payment")) {
-     popForm.style.display = 'block'
-      
     }
 
-      closeForm.addEventListener ("click", (e)=> {
-       e.preventDefault()
-        popForm.style.display = "none"
-     })
-     })
+    let paymentButton = document.querySelector(".test")
+    console.log(paymentButton)
+    let popForm = document.querySelector("#payment-form")
+    console.log(popForm)
+    let closeForm = document.querySelector("#submit")
+    console.log(closeForm)
 
-     popForm.addEventListener ("submit", (e)=> {
-   e.preventDefault()
-   const input1 = e.target.name.value
-   const input2 = e.target.email.value
-   const input3= e.target.car_id.value
+    listProduct.addEventListener("click", (event) => {
+        event.preventDefault()
+        let positionClick2 = event.target
+        console.log(positionClick2)
+        if (positionClick2.classList.contains("payment")) {
+            popForm.style.display = 'block'
 
-   console.log(input1)
-   console.log(input2)
-   console.log(input3)
+        }
 
-   fetch ("http://localhost:3000/Buyers", {
-    method: "POST",
-    headers : {
-        "Content-Type" : "application/json"
-    },
-    body : JSON.stringify ({
-        name : input1,
-        email: input2,
-        car_id : input3
+        closeForm.addEventListener("click", (e) => {
+            e.preventDefault()
+            popForm.style.display = "none"
+        })
     })
-   })
-   .then (res => res.json())
-   .then (data => console.log(data))
-//    popForm.reset ()
-     })
-     
+
+    popForm.addEventListener("submit", (e) => {
+        e.preventDefault()
 
 
-// add event listeners to the "buy car buttons in the sports car section"
-    //  listSport.addEventListener("click", (event)=> {
-    //     event.preventDefault()
-    //     let positionClick3 = event.target
-    //      console.log(positionClick3)
-    //    if (positionClick3.classList.contains ("payment")) {
-    //      popForm.style.display = 'block'}
-    
-    //       closeForm.addEventListener ("click", (e)=> {
-    //        e.preventDefault()
-    //         popForm.style.display = "none"
-    //      })
-    //      })
-    
-    //      popForm.addEventListener ("submit", (e)=> {
-    //    e.preventDefault()
-    //    const input1 = e.target.name.value
-    //    const input2 = e.target.email.value
-    //    const input3= e.target.car_id.value
-    
-    //    console.log(input1)
-    //    console.log(input2)
-    //    console.log(input3)
-    
-    //    fetch ("http://localhost:3000/Buyers", {
-    //     method: "POST",
-    //     headers : {
-    //         "Content-Type" : "application/json"
-    //     },
-    //     body : JSON.stringify ({
-    //         name : input1,
-    //         email: input2,
-    //         car_id : input3
-    //     })
-    //    })
-    //    .then (res => res.json())
-    //    .then (data => console.log(data))
-    // //    popForm.reset ()
-    //      })
 
- // adding event listeners to the add to wishlist buttons in the sports car section
+        const input1 = e.target.name.value
+        const input2 = e.target.email.value
+        const input3 = e.target.car_id.value
 
-    //      listSport.addEventListener("click", (event)=> {
-    //         event.preventDefault()
-    //         let positionClick4 = event.target
-    //          //console.log(positionClick)
-    //        if (positionClick4.classList.contains ("wishlist")) {
-    //          wishlistForm.style.display = 'block'}
-    
-    //           closeWishlistForm.addEventListener ("click", (e)=> {
-    //            e.preventDefault()
-    //             wishlistForm.style.display = "none"
-    //          })
-    
-    //          let form = document.querySelector ("#wishlist-form")
-    //          form.addEventListener("submit", (e)=> {
-    //             e.preventDefault()
-    //             const input = document.querySelector("input#wishlisttitle")
-    //             id = input.value
-    //             fetch (`http://localhost:3000/Sports/${input.value}`)
-    //             .then (res => res.json())
-    //             .then  (data => {
-    //                 console.log(data)
-    //             addWishlistItem (data,id)
-    //             renderWishlistItem (data)
-    //         }
-    //         )
-    //             form.reset ()
-    
-    //         })
-         
-    // })
+
+
+        fetch("http://localhost:3000/Buyers", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                name: input1,
+                email: input2,
+                car_id: input3
+            })
+        })
+            .then(res => res.json())
+            .then(data => console.log(data))
+
+    })
+
+
+
+
+
 })
